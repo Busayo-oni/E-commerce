383d2e48fe5a61e31edefa0ad4ce0333d800ae90
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2024-11-12 21:00:28 +0100

    version 1.0

--- vite-project/src/component/Hero.jsx
+++ vite-project/src/component/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Hero.css';
 import heroImage from '../assets/pexels-minan1398-793010.jpg'; // Import image
 
-const HeroSection = () => {
+const HeroSection = () => { 
   return (
     <section className="hero">
       <div className="hero-image">

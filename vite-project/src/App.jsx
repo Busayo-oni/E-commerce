9518fcdd7e984390920fde9d92f8fc1c7e3563a0
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2024-11-12 20:42:36 +0100

    version 01

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Exclusive from './component/exclusive'
 import './App.css'
-import { OurStory } from './component/OurStory'
+import OurStory from './component/OurStory'
+import Header from './component/Header'
+import Sea from './component/Sea'
+import Footer from './component/Footer'
+import Flavor from './component/Flavor'
+
 
 function App() {
 
   return (
     <>
       <Header />
-      <Hero />
+      {/* <Hero /> */}
       <OurStory />
+      <Flavor />
       <Sea />
       <Exclusive />
       <Footer />
